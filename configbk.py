e4209f2f514d90c9ead381303a861c48cdd3caee
m87 <m87blackhole@gmail.com>
2014-10-24 00:54:24 +0200

    init

--- configbk.py
+++ configbk.py
@@ -3,7 +3,6 @@
 
 import jsonpickle
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
-
 class Item(object):
     def __init__(self, id, src, dst):
         self.id = id
@@ -11,10 +10,11 @@ class Item(object):
         self.dst = dst
 
 
-
-
 if __name__ == '__main__':
     parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
     parser.add_argument("-b", "--backup", type=str, default="config.backup", help="Create/update backup")
+    parser.add_argument("-u", "--update", type=str, default="config.backup" ,help="Update configs")
+    parser.add_argument("-r", "--restore", type=str, default="config.backup" ,help="Restore configs")
+
 
 args = parser.parse_args()
